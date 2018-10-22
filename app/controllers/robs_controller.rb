d89e2d620841720b1e3e501a954911207d51843e
Augustin NSENGIYUMVA <nsau2020@gmail.com>
2018-10-22 20:51:33 +0000

    actionmailer

--- app/controllers/robs_controller.rb
+++ app/controllers/robs_controller.rb
@@ -108,7 +108,7 @@ class RobsController < ApplicationController
   private
   #Before_action : Set_rob, only: [:show, :edit, :update, :destroy] 
     def rob_params
-    params.require(:rob).permit(:title, :content, :image).merge(user: current_user)
+    params.require(:rob).permit(:title, :content).merge(user: current_user)
    end
   
   
